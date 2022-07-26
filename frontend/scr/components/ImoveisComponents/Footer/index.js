490d20b1a3fb2da70ab36ece93c324067277054d
Rúbia Marques de Oliveira <rubiamarquesoliveira@gmail.com>
2022-07-25 22:47:50 -0300

    Create index.js

---
+++ frontend/scr/components/ImoveisComponents/Footer/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    background-color:#000d1a;
+    color:#eee;
+    padding: 1vw;
+    font-family: 'Montserrat',sans-serif;
+    max-height: 20%!important;
+    min-height: 20%!important;
+    @media screen and (max-width: 763px){
+        //margin-top: 30%;
+    }
+`;
+const Row = styled.div`
+    display: flex;
+    justify-content: space-between;
+    height: 100%;
+    bottom: 0;
+    left: 0;
+`;
+const Column = styled.div`
+    //margin-right: 10px;
+`;
+const Title = styled.h4`
+    margin-left: 40px;
+    @media screen and (max-width: 763px){
+        font-size: 100%;
+    }
+`;
+const Lista = styled.ul`
+
+`;
+const Item = styled.li`
+    list-style:none;
+    
+    @media screen and (max-width: 763px){
+        font-size: 60%;
+    }
+`;
+
+export const Footer = () => {
+    return (
+        <Div>
+            <Row>
+                <Column>
+                    <Title>Vida Imóveis</Title>
+                    <Lista>
+                        <Item>Compra</Item>
+                        <Item>Venda</Item>
+                        <Item>Locação</Item>
+                    </Lista>
+                </Column>
+                <Column>
+                    <Title>Atendimento</Title>
+                    <Lista>
+                        <Item>Segunda a Sexta: 08:00 - 16:00</Item>
+                        <Item>Sábado - 08:00 - 12:00</Item>
+                    </Lista>
+                </Column>
+                <Column>
+                    <Title>Telefones</Title>
+                    <Lista>
+                        <Item>(37) 99999 - 9999</Item>
+                        <Item>(37) 99999 - 9999</Item>
+                    </Lista>
+                </Column>
+            </Row>
+        </Div>
+    )
+}
