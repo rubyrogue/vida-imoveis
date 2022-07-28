efa4a69cdab79b57971245897a9b768a5c6b15f7
Rúbia Marques de Oliveira <rubiamarquesoliveira@gmail.com>
2022-07-27 21:38:52 -0300

    Create index.js

---
+++ frontend/scr/data/SliderData/index.js
@@ -0,0 +1,73 @@
+import Img1 from '../../images/img1.jpg';
+import Img2 from '../../images/img3.jpg';
+import Img3 from '../../images/img4.jpg';
+import Img4 from '../../images/img666.jpg';
+import IgmGreen from '../../images/IgmGreen.jpg';
+
+export const SliderData = [
+    {title: 'Casa residencial de luxo',
+     price: '2000000,00',
+     path: '/imoveis',
+     label: 'Visualizar',
+     image: Img1,
+     alt: 'House'
+     },
+
+     {title: 'Mansão',
+      price: '3000000,00',
+      path: '/imoveis',
+      label: 'Visualizar',
+      image: Img2,
+      alt: 'House'
+     },
+
+     {title: 'Casa residencial',
+      price: '3500000,00',
+      path: '/imoveis',
+      label: 'Visualizar',
+      image: Img3,
+      alt: 'House'
+     },
+
+     {title: 'Casa',
+      price: '2000000,00',
+      path: '/imoveis',
+      label: 'Visualizar',
+      image: Img4,
+      alt: 'House'
+     },
+]
+
+export const SliderData2 = [
+    {title: 'Casa residencial de luxo',
+     price: '2000000,00',
+     path: '/imoveis',
+     label: 'Visualizar',
+     image: IgmGreen,
+     alt: 'House'
+     },
+
+     {title: 'Mansão',
+      price: '3000000,00',
+      path: '/imoveis',
+      label: 'Visualizar',
+      image: Img2,
+      alt: 'House'
+     },
+
+     {title: 'Casa residencial',
+      price: '3500000,00',
+      path: '/imoveis',
+      label: 'Visualizar',
+      image: Img3,
+      alt: 'House'
+     },
+
+     {title: 'Casa',
+      price: '2000000,00',
+      path: '/imoveis',
+      label: 'Visualizar',
+      image: Img4,
+      alt: 'House'
+     },
+]
