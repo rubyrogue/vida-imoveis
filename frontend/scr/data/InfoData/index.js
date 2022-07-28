b68a0a89898dfff395884898d5337548799d3612
Rúbia Marques de Oliveira <rubiamarquesoliveira@gmail.com>
2022-07-27 21:38:04 -0300

    Create index.js

---
+++ frontend/scr/data/InfoData/index.js
@@ -0,0 +1,23 @@
+import Img1 from '../../images/imgIn3.jpg';
+import Img2 from '../../images/imgIn.jpg';
+
+export const InfoData1 = {
+    heading: 'Explore imóveis perfeitos para você!',
+    parag1: 'Um local super aconchegante para você e para toda a sua família.',
+    parag2: 'Um local super aconchegante para você e para toda a sua família.',
+    buttonLabel: 'Visualizar imóveis',
+    image: Img1,
+    reverse: true,
+    delay: 100
+}
+
+export const InfoData2 = {
+    heading: 'Seu lar perfeito a um click de você!',
+    parag1: 'Um local super aconchegante para você e para toda a sua família.',
+    parag2: 'Um local super aconchegante para você e para toda a sua família.',
+    buttonLabel: 'Visualizar imóveis',
+    image: Img2,
+    reverse: false,
+    delay: 300
+}
+
