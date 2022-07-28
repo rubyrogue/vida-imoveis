a04a801c71a51111539c45cc75dfdc61d79f63ef
Rúbia Marques de Oliveira <rubiamarquesoliveira@gmail.com>
2022-07-27 21:42:09 -0300

    Create AddImoveis.js

---
+++ frontend/scr/pages/AddImoveis.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/AddImovel/Header';
+import Body from '../components/AddImovel/Body';
+
+export const AddImoveis = () => {
+    return (
+        <>
+            <Header/>
+            <Body/>
+        </>
+    )
+}
